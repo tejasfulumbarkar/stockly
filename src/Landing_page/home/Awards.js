bfd0efdc6126e6e6098c0cd5d80fb2531e79aba5
Tejas Fulumbarkar <tejasshyamfulumbarkar@gmail.com>
2026-02-13 21:13:05 +0530

    hero and awards component done

--- src/Landing_page/home/Awards.js
+++ src/Landing_page/home/Awards.js
@@ -1,8 +1,41 @@
 import React from 'react'
+import './Awards.css'
 function Awards() {
     return (
+        <div className='container big mt-5'>
+            <div className='row'>
+                <div className='col-6'>
+                    <img src='/media/images/largestBroker.svg' alt='' />
 
-        <h1>hii i am awards func</h1>
+                </div>
+                <div className='col-6'>
+                    <h1>
+                        Largest Stock Broker in India
+                    </h1>
+                    <p className='fs-5'>
+                        2+ million Stockly clients contribute to over 15% of all retail order volumes in India daily by trading and investing in :
+                    </p>
+                    <div className="container mt-5">
+  <ul className="big-dots row">
+    <li className="col-6">Future and Options</li>
+    <li className="col-6">Stocks and IPOs</li>
+
+    <li className="col-6">Commodity Derivatives</li>
+    <li className="col-6">Direct Mutual Funds</li>
+
+    <li className="col-6">Currency Derivatives</li>
+    <li className="col-6">Bonds and Governance</li>
+  </ul>
+</div>
+<img src='media/images/pressLogos.png' alt=''/>
+
+
+                </div>
+
+            </div>
+
+        </div>
+
       );
 }
 
--- src/Landing_page/home/Hero.js
+++ src/Landing_page/home/Hero.js
@@ -1,8 +1,18 @@
 import React from 'react'
+import './Hero.css'
 function Hero() {
     return (
 
-        <h1>hii i am hero func</h1>
+        <div className='container p-5'>
+            <div className='row text-center'>
+               <img src='media/images/stocklyhero.png' alt='' className='mb-5'></img>
+        <h1 className='mt-5'> Invest in Everything</h1>
+        <p className='fs-5'>Online platform to invest in stocks, derivatives, mutual funds, ETFs, bonds, and more.</p>
+        <button className='btn btn-primary my-btn'>Sign Up for free</button>
+            </div>
+
+        </div>
+
       );
 }
 
